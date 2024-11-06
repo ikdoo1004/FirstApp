afffbe4fcb217fae7503124f5b05f8a34a519e46
Ikdoo Kim <Ikdoo1004@gmail.com>
2024-11-06 09:44:27 -0500

    Initial Commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by 김익두 on 11/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
